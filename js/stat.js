dc7f09850db499c76d44a7e7fdfd83795bd8f60e
RDN <RDemian@github.com>
2018-06-05 16:57:53 +0500

    изменения согласно замечаниям

--- js/stat.js
+++ js/stat.js
@@ -1,28 +1,37 @@
 'use strict';
-var CLOUD_WIDTH = 300;
-var CLOUD_HEIGHT = 180;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var BORDER_GAP = 30;
-var GAP = 10;
+var BORDER_GAP = 50;
+var GAP = 20;
 var FONT_HEIGHT = 15;
-var TEXT_WIDTH = 52;
+var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
 var maxBarHeight = 50;
 var winnerIndex = 0;
 var player = {
   name: '',
-  playerIndex: 0,
-  playerTime: 0,
+  index: 0,
+  time: 0,
   maxTime: 0,
   minTime: 0
 };
 
 var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(x + 10, y + 10, CLOUD_WIDTH, CLOUD_HEIGHT);
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  var i = 0;
+  // в первый проход отрисовывается тень облака, второй проход само облако.
+  for (i = 0; i < 2; i += 1) {
+    ctx.fillStyle = (i === 0) ? 'rgba(0, 0, 0, 0.7)' : color;
+    ctx.beginPath();
+    x = (i === 0) ? x + 10 : x - 10;
+    y = (i === 0) ? y + 10 : y - 10;
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + CLOUD_WIDTH, y);
+    ctx.bezierCurveTo(x + CLOUD_WIDTH - 30, y + CLOUD_HEIGHT / 3, x + CLOUD_WIDTH + 35, y + CLOUD_HEIGHT / 2, x + CLOUD_WIDTH, y + CLOUD_HEIGHT);
+    ctx.lineTo(x, y + CLOUD_HEIGHT);
+    ctx.fill();
+  }
 };
 
 var renderBar = function (ctx, x, y, currentPlayer) {
@@ -30,18 +39,18 @@ var renderBar = function (ctx, x, y, currentPlayer) {
   maxBarHeight = CLOUD_HEIGHT - (CLOUD_Y + BORDER_GAP + FONT_HEIGHT) - 2 * (GAP + FONT_HEIGHT);
   var koefDiagramm = Math.round(currentPlayer.minTime / 1.2);
   var maxTime = Math.round(currentPlayer.maxTime - koefDiagramm);
-  var barHeight = Math.round(maxBarHeight * (currentPlayer.playerTime - koefDiagramm) / maxTime);
+  var barHeight = Math.round(maxBarHeight * (currentPlayer.time - koefDiagramm) / maxTime);
   // Рисуем имя игрока
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText(currentPlayer.name, x + 1, y);
   // рисуем столбик диаграммы
-  ctx.fillStyle = 'hsl(' + currentPlayer.playerIndex * 62 + ' , 95%, 46%)';
+  ctx.fillStyle = (currentPlayer.name === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + (10 + (currentPlayer.index * 30)) + '%, 60%)';
   y = y - barHeight - FONT_HEIGHT;
   ctx.fillRect(x, y, BAR_WIDTH, barHeight);
   // рисуем сверху время игрока
   ctx.fillStyle = '#000';
-  ctx.fillText(Math.round(currentPlayer.playerTime / 10) / 100, x, y - GAP / 2);
+  ctx.fillText(Math.round(currentPlayer.time / 10) / 100, x, y - GAP / 2);
 };
 
 window.renderStatistics = function (ctx, names, times) {
@@ -81,8 +90,8 @@ window.renderStatistics = function (ctx, names, times) {
     barX = CLOUD_X + BORDER_GAP + GAP * i + TEXT_WIDTH * i;
     barY = CLOUD_Y + CLOUD_HEIGHT - BORDER_GAP / 2;
     player.name = names[i];
-    player.playerIndex = i;
-    player.playerTime = Math.round(times[i]);
+    player.index = i;
+    player.time = Math.round(times[i]);
     player.maxTime = Math.round(maxTime);
     player.minTime = Math.round(minTime);
     renderBar(ctx, barX, barY, player);
